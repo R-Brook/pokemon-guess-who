6ae61dc26d88040ce02a33575464991c58116181
R Brook <rlbrook@gmail.com>
2024-12-16 21:50:31 +0100

    Use Apollo Client for GraphQL.

---
+++ apollo-graphql/queries/pokemons.ts
@@ -0,0 +1,27 @@
+import { gql } from "@apollo/client"
+
+export const POKEMONS_QUERY = gql`
+  query Pokemons {
+    pokemon_v2_pokemon(limit: 40) {
+      height
+      base_experience
+      name
+      id
+      weight
+      pokemon_v2_pokemonstats {
+        base_stat
+        pokemon_v2_stat {
+          name
+        }
+      }
+      pokemon_v2_pokemontypes {
+        pokemon_v2_type {
+          name
+        }
+      }
+      pokemon_v2_pokemonsprites {
+        sprites
+      }
+    }
+  }
+`
---
+++ types/pokemons.ts
@@ -0,0 +1,39 @@
+export interface PokemonsProps {
+  pokemonsData: pokemonsData[]
+}
+
+export interface pokemonsData {
+  height: number
+  base_experience: number
+  name: string
+  id: number
+  weight: number
+  pokemon_v2_pokemonstats: pokemonStats[]
+  pokemon_v2_pokemontypes: pokenmonTypes[]
+  pokemon_v2_pokemonsprites: pokemonSprites[]
+}
+
+export interface pokemonStats {
+  base_stat: number
+  pokemon_v2_stat: pokemonStat[]
+}
+
+export interface pokemonStat {
+  name: string
+}
+
+export interface pokenmonTypes {
+  pokemon_v2_type: {
+    name: string
+  }
+}
+
+export interface pokemonSprites {
+  sprites: {
+    other: {
+      "official-artwork": {
+        front_default: string
+      }
+    }
+  }
+}
