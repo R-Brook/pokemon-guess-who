c519a9f7c1726c3e441072ded91013432282347a
R Brook <rlbrook@gmail.com>
2024-12-19 18:43:55 +0100

    Tidy up Next.js and TS config.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,19 +1,10 @@
-import type { ReactElement, ReactNode } from "react"
-import type { NextPage } from "next"
 import type { AppProps } from "next/app"
 import "../css/styles.css"
 
-export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
-  getLayout?: (page: ReactElement) => ReactNode
-}
-
-type AppPropsWithLayout = AppProps & {
-  Component: NextPageWithLayout
-}
-
-export default function MyApp({ Component, pageProps }: AppPropsWithLayout) {
-  // Use the layout defined at the page level, if available
-  const getLayout = Component.getLayout ?? ((page) => page)
-
-  return getLayout(<Component {...pageProps} />)
+export default function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <main className="bg-slate-100">
+      <Component {...pageProps} />
+    </main>
+  )
 }
